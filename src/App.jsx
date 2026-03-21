aaa6935112f928750576b7742c3ec3692332fa67
marcozanda82 <marco.zanda@gmail.com>
2026-03-21 20:07:56 +0100

    Merge remoto + fix3 Modulo 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import SalaComandi from './SalaComandi';
-import ReadyCoreSplash from './ReadyCoreSplash';
 
 export default function App() {
-  const [isBooting, setIsBooting] = useState(true);
-
-  useEffect(() => {
-    if (isBooting) {
-      document.body.style.overflow = 'hidden';
-      document.documentElement.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-      document.documentElement.style.overflow = '';
-    }
-    return () => {
-      document.body.style.overflow = '';
-      document.documentElement.style.overflow = '';
-    };
-  }, [isBooting]);
-
-  if (isBooting) {
-    return <ReadyCoreSplash onComplete={() => setIsBooting(false)} />;
-  }
-
   return <SalaComandi />;
 }
