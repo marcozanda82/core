e1ada4ee6358144ec71d05610b72b8f6eaa27361
marcozanda82 <marco.zanda@gmail.com>
2026-05-03 12:40:10 +0200

    feat: add and validate SleepCoachCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import SalaComandi from './SalaComandi';
-import SleepCoachDebug from '@/dev/SleepCoachDebug';
 
 export default function App() {
-  return <SleepCoachDebug />;
-  // return <SalaComandi />;
+  return <SalaComandi />;
 }
